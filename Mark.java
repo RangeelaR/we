45730b7d957507f5adf922708696c4cbbb0c3b5a
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-01-20 01:09:23 +0530

    Create Mark.java

---
+++ Mark.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Mark {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		System.out.println("enter the number");
+		int n=sc.nextInt();
+    for(int i=0;i<n;i++)
+    {
+    System.out.println("Hello");
+    }
