ee169df4657d829a512df671f1f2da15afc828f2
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-01-20 01:03:26 +0530

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Leap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		int a=sc.nextInt();
+		double b=a%4;
+		if(b==0){
+		System.out.println("leap year");
+		}
+		else
+		{
+		
+		
+		System.out.println("not leap year");
+		
+	}}}
+
+
