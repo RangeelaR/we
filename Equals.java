1d620d828d81f22c7023c0681da6c2fc57763eca
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-01-20 22:10:36 +0530

    Create Equals.java

---
+++ Equals.java
@@ -0,0 +1,18 @@
+public class Equals {
+
+	public static void main(String[] args) {
+	
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("enter 1 word is ");
+		String s,s1,s2;
+		s=sc.nextLine();
+		System.out.println("enter 2 word is ");
+	    s1=sc.nextLine();
+	    boolean a =s.equalsIgnoreCase(s1);
+	    System.out.println("compare without consider case "+a);
+	    
+
+	}
+
+}
