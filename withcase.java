971ecfa002f98eb78711493fa3f0efad6b528c36
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-01-21 00:44:58 +0530

    Create withcase.java

---
+++ withcase.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+public class withcase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("enter 1 word is ");
+		String s,s1,s2;
+		s=sc.nextLine();
+		System.out.println("enter 2 word is ");
+	    s1=sc.nextLine();
+	    
+	    boolean c =s.equals(s1);
+	    System.out.println("compare with consider case "+c);
+      
+
+	}
+
+}
