0dd89b9cb959bfb368ce1c41cbb7aa816df1b2a8
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-01-20 23:42:45 +0530

    Create triangle.java

---
+++ triangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the values");
+		int l = sc.nextInt();
+		int b = sc.nextInt();
+		double a=0.5*l*b;
+		System.out.println(a);
+	
+
+	}
+
+}
