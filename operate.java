85b9ca7fd8c634ff1e2760be31c2e633b01615e7
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-01-21 00:01:24 +0530

    Create operate.java

---
+++ operate.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class operate{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the values");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		double r=(a^b)%c ;
+		System.out.println(r);
+	
+
+	}
+
+}
