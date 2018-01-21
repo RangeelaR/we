ff99ee7327d2d622978b9522589e8bcf500701b9
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-01-21 09:49:12 +0530

    Update factor.java

--- factor.java
+++ factor.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class factor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter the values");
 		int m = sc.nextInt();
-		int sum=1;
+		int s=1;
 		for(int i=1;i<=m;i++)
 		{
-			sum=sum*i;
+			s=s*i;
 		}
-		System.out.println(sum);
+		System.out.println(s);
 	}
 	}
