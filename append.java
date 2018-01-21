9e5612ee292cb851b8015e13233545cc8cd7de6c
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-01-21 09:30:13 +0530

    Create append.java

---
+++ append.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class append {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the values");
+		String s=sc.nextLine();
+		System.out.println(s+".");
+		}
+}
