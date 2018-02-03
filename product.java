6efbd65a7eb4e44cd00d0e2938d303589ad5ff6e
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-02-03 11:00:37 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class product {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the values");
+		int m = sc.nextInt();
+		int sum=1;
+		for(int i=1;i<=m;i++)
+		{
+			sum=sum*i;
+		}
+		System.out.println(sum);
+	}
+	}
