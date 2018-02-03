ab66ddb304c072f89ce3a4ee3f498d45dcc75d40
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-02-03 11:11:43 +0530

    Create hexa.java

---
+++ hexa.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class hexa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		 System.out.println("enter the number ");
+		 int s=sc.nextInt();
+		 String b=Integer.toHexString(s);
+		 System.out.println("the number "+b);
+		 
+
+	}
+
+}
