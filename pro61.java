d14ef9db8848209dba249f4bb4d9db5ed7713dd6
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-02-06 06:23:56 +0000

    Create pro61.java

---
+++ pro61.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class pro61 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	        Scanner s = new Scanner(System.in);
+	        System.out.print("Enter:");
+	        String n = s.nextLine();
+          int n1=n.length();
+          int ascii[]=new int[n1];
+        
+          char c[]=n.toCharArray ;
+          for(i=o;i<n1;i++)
+          {
+           ascii[i]=c[i]
+          }
+           System.out.print("Enter ");
+         String n1 = s.nextLine();
+          int n11=n.length();
+          int asci[]=new int[n11];
+        int b[i]=new int =[n11];
+          char c[]=n.toCharArray ;
+          for(i=o;i<n11;i++)
+          {
+           asci[i]=c[i]
+            b[i]=asci[i]-97
+           
+          }
+          int d[]=new int[n11];
+          for(i=o;i<n11;i++) 
+          {
+          d[i]=ascii[i]+b[i]
+          c[i]=char(d[i])
+         System.out.print("encoded"+c[i]); 
+          }
+          
