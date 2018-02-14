8e6fe5b93ce17f700fb4eac9db3c50d7556b3c04
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-02-14 21:29:43 +0530

    Create beg104.java

---
+++ beg104.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class beg104 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number :");
+		int n=sc.nextInt();
+		System.out.println("Enter the number :");
+		int k=sc.nextInt();
+		double t= Math.pow(n,k);
+		System.out.println("the result :"+t);
+
+	}
+
+}
