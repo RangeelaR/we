d8433b0c9d1e3f0c87edab98fa6f0fc228669d85
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-02-14 21:43:57 +0530

    Create BEG93.java

---
+++ BEG93.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BEG93 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the celsius");
+		double n=sc.nextInt();
+		double k=n+273.15;
+		System.out.println("In kelvin "+k);
+	}
+
+}
