830cbd17d56b0766173d1c6ca5e74b49a00761ea
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-02-14 21:30:52 +0530

    Create beg106.java

---
+++ beg106.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class beg106 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the ");
+		int a=sc.nextInt();
+		int b= 96+a;
+		char c= (char)b;
+        System.out.println("The char of " + a + " is: " + c);
+
+
+	}
+
+}
