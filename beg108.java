cc61a6df2edb9ec4d0eb862008abf025646d6d31
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-02-14 21:32:42 +0530

    Create beg108.java

---
+++ beg108.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class beg108 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the first element");
+		int a=sc.nextInt();
+		System.out.println("enter the number");
+		int n=sc.nextInt();
+		System.out.println("enter the difference");
+		int d=sc.nextInt();
+		int b=a+(n-1)*d;
+		int sum=(n/2)*(a+b);
+		System.out.println("result is "+sum);
+
+	}
+
+}
