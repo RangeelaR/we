ddbe3336ee14026a4b394102c4168da982d37ac7
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-02-14 21:40:15 +0530

    Create beg116.java

---
+++ beg116.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class beg116 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number :");
+		int s=sc.nextInt();
+		
+		String val = String.valueOf(s);
+	
+		int s1=sc.nextInt();
+		
+		String al1 = String.valueOf(s1);
+		String val1=val+al1;
+		System.out.println("the number:"+val1);
+
+	}
+
+}
