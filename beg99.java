4b4b7479245ce69b94ad797fb44d84d2b2a1a872
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-02-14 21:41:02 +0530

    Create beg99.java

---
+++ beg99.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class beg99 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the no of element");
+		int a=sc.nextInt();
+		System.out.println("enter the no of element");
+		int b=sc.nextInt();
+		System.out.println("enter the no of element");
+		int c=sc.nextInt();
+		int d=(a*b)%c;
+		System.out.println("result : "+d);
+
+	}
+
+}
